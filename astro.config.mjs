25c378e776661044d5df053830e52174b61f5854
Mikhail Potapov <mpotapov.it@gmail.com>
2024-07-20 15:49:30 +0700

    Add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://glittery-lamington-2ebed2.netlify.app"
+  site: "https://glittery-lamington-2ebed2.netlify.app",
+  integrations: [preact()]
 });
