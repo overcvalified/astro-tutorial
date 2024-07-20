2fbf87104112ee94265b9b1575512b0a03278f06
Mikhail Potapov <mpotapov.it@gmail.com>
2024-07-20 15:35:37 +0700

    Add rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://glittery-lamington-2ebed2.netlify.app"
+});
